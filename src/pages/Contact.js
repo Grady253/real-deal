4c5dea53aad5aa05ebf2e6ce6e25e90318cf8fc0
Grady253 <grady.andre2@gmail.com>
2022-12-20 13:44:43 -0800

    added downloadable resume button.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,7 @@
 import { Container, Heading, Center, Text} from "@chakra-ui/layout";
+import pdf from "../utilities/Andre_Grady_Resume.pdf";
 import React from "react";
+import { Button} from "@chakra-ui/react";
 
 
 export default function Contact(){
@@ -12,7 +14,13 @@ export default function Contact(){
                 <Text fontSize='25px'>
                     I'd love to get in touch! Here are my socials below.
                 </Text>
-               
+            </Center>
+            <Center>
+                <Button>
+                <Text fontSize='20px'>
+                    <a style={{textDecoration:'none'}}href={pdf} download><Text color='black' fontSize='lg'>Download Resume</Text></a>
+                </Text>
+                </Button>
             </Center>
         </Container>
     )
